38ed69ba2df22574a33426843694fca65286cf0d
Barney Moss <barneym@meta.com>
2024-12-03 08:31:06 +0000

    Day 3: regex, quite pleased with this!

--- day03/day03.py
+++ day03/day03.py
@@ -1,1 +1,15 @@
 # https://adventofcode.com/2024/day/3
+
+
+import re
+
+
+def q1(input):
+    total = 0
+    for m in re.finditer(r'mul\((\d{1,3}),(\d{1,3})\)', ''.join(input)):
+        total = total + int(m.group(1)) * int(m.group(2))
+    return total
+
+def q2(input):
+    cleaned = re.sub(r"don't\(\).*?do\(\)", '', ''.join(input))
+    return q1(cleaned.splitlines())
