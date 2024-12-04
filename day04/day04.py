8da5e6419f94fba2d6fcf3134f21b3436c00054a
Barney Moss <barneym@meta.com>
2024-12-04 11:06:01 +0000

    Optimisation to Day 4

--- day04/day04.py
+++ day04/day04.py
@@ -5,6 +5,8 @@ def q1(lines):
     count = 0
     for y in range(len(lines)):
         for x in range(len(lines[0])):
+            if lines[y][x] != "X":
+                continue
             for dx, dy in [
                 (0, 1),
                 (0, -1),
