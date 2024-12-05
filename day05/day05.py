3afa620ccccc98c4e8ad14a058b0c81ed41dbf7f
Barney Moss <barneym@meta.com>
2024-12-05 08:33:51 +0000

    Day 5: sorting

--- day05/day05.py
+++ day05/day05.py
@@ -1,1 +1,45 @@
 # https://adventofcode.com/2024/day/5
+
+from functools import cmp_to_key, partial
+
+def q1(lines):
+    orderings = [list(map(int, l.split("|"))) for l in lines if "|" in l]
+    sections = [list(map(int, l.split(","))) for l in lines if "," in l]
+
+    sum_middles = 0
+    for s in sections:
+        if is_ordered(s, orderings):
+            # print(f"{s} is ordered, middle is {s[int(len(s)/2)]}")
+            sum_middles += s[int(len(s)/2)]
+    return sum_middles
+
+
+def is_ordered(pages, orderings):
+    for lo, hi in orderings:
+        try:
+            if pages.index(lo) > pages.index(hi):
+                # print(f'{pages} disordered: {lo} > {hi}')
+                return False
+        except ValueError:
+            continue
+    return True
+
+
+def q2(lines):
+    orderings = [list(tuple(map(int, l.split("|")))) for l in lines if "|" in l]
+    sections = [list(map(int, l.split(","))) for l in lines if "," in l]
+
+    sum_middles = 0
+    for s in sections:
+        if is_ordered(s, orderings):
+            continue
+        o = sorted(s, key=cmp_to_key(partial(key_func, orderings)))
+        # print(f"{s} => {o}, middle is {o[int(len(o)/2)]}")
+        sum_middles += o[int(len(o)/2)]
+    return sum_middles
+
+
+def key_func(orderings, l, r):
+    if [r, l] in orderings:
+        return 1
+    return -1
